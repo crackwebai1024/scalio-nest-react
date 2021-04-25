08be13f620048d2d9dac74dbea3e3c913887601e
crackwebai1024 <webtowardapex@outlook.com>
2021-04-26 02:44:16 +0800

    complete testing

--- backend/src/posts/posts.dto.ts
+++ backend/src/posts/posts.dto.ts
@@ -1,6 +1,6 @@
 export class PostDto {
-  readonly UserId: number;
-  readonly Id: number;
+  readonly userId: number;
+  readonly id: number;
   readonly title: string;
   readonly body: string;
 }
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PostDto } from 'src/posts/posts.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,41 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/posts (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/posts')
+      .then((res) => {
+        let data: PostDto[] = JSON.parse(res.text);
+        let str: string =
+          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit';
+        expect(data.length).toBe(10);
+        expect(data[0].id).toBe(1);
+        expect(data[0].userId).toBe(1);
+        expect(data[0].title).toBe(str);
+      });
+  });
+
+  it('/posts/:id (GET)', () => {
+    let str: string =
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit';
+    return request(app.getHttpServer())
+      .get('/posts/1')
+      .then((res) => {
+        let data: PostDto = JSON.parse(res.text);
+        expect(data.id).toBe(1);
+        expect(data.userId).toBe(1);
+        expect(data.title).toBe(str);
+      });
+  });
+
+  it('/posts/:id (GET)', () => {
+    let str: string =
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit';
+    return request(app.getHttpServer())
+      .get('/posts/30')
+      .then((res) => {
+        let data = JSON.parse(res.text);
+        expect(data.message).toBe('Not Found');
+      });
   });
 });
